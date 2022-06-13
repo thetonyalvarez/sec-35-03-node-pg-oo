dce4c1c11e61dc6b48f5e7bd493544f4bf0d5942
Tony Alvarez <tonyalva87@gmail.com>
2022-06-12 19:40:00 -0700

    setup of write-customers-test branch

--- db.js
+++ db.js
@@ -1,8 +1,13 @@
 /** Database for lunchly */
 
-const pg = require("pg");
+const DB_URI =
+	process.env.NODE_ENV === "test"
+		? "postgresql:///lunchly_test"
+		: "postgresql:///lunchly";
 
-const db = new pg.Client("postgresql:///lunchly");
+let db = new Client({
+	connectionString: DB_URI,
+});
 
 db.connect();
 
